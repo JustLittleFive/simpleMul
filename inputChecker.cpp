a2b0b113849b3bf0b191fc7defed055d85da19c5
-l <newyue2010@163.com>
2022-09-22 11:19:29 +0800

    Checker WIP

---
+++ inputChecker.cpp
@@ -0,0 +1,95 @@
+/*
+ Copyright 2022 Google LLC
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+    string str = "";
+    cin >> str;
+
+    bool isNegative = 0;
+    if (str[0] == '-')
+    {
+        isNegative = 1;
+        str.erase(0, 1);
+    }
+
+    const char* start = str.c_str();
+
+    int len = str.length();
+
+    char *array = new char[len];
+
+    int i = 0;
+    int l = 0;
+
+
+
+    int countE = count(str.begin(), str.end(), 'E');
+    int counte = count(str.begin(), str.end(), 'e');
+    int countP = count(str.begin(), str.end(), '.');
+    int countN = count(str.begin(), str.end(), '-');
+
+    
+    int indexE = -1;
+    int flocate = -1;
+
+    for (i = 0, l = 0; l < len; i++, l++)
+    {
+        if ((start[l] < 45 || start[l] == 47)
+            || (start[l] > 57 && start[l] != 101))
+        {
+            cout << "Invalid input!" << endl;
+            return 0;
+        }
+         switch (start[l])
+        {
+        case 'e'  /* constant-expression */:
+            /* code */
+            countE++;
+            indexE = l;
+            continue;
+
+        case '.':
+            countP++;
+            flocate++;
+            i--;
+            continue;
+
+        case '-':
+            countN++;
+            continue;
+
+        default:
+            array[i] = start[l];
+        }
+        if (flocate > -1)
+        {
+            flocate++;
+        }
+        if (countE > 1 || countN > 2 || countP > 1)
+        {
+            cout << "Invalid input!" << endl;
+            return 0;
+        }
+    }
+
+}
