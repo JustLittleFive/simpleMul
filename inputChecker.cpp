565cd2bc5d7894613655e41e566bfb0af8b78935
-l <newyue2010@163.com>
2022-09-24 22:29:21 +0800

    Early stage COMPLETE

--- inputChecker.cpp
+++ inputChecker.cpp
@@ -16,20 +16,32 @@
 
 #include <algorithm>
 #include <tuple>
-#include <components.hpp>
+#include <cstring>
+#include <iostream>
 
 using namespace std;
 
-int main(int argc, char *argv[])
+/// @brief  Convert string to char[]
+/// @param str: string
+/// @return char *: the start of char[]
+char *str2charl(string str)
 {
-    string str1 = argv[1];
-    string str2 = argv[2];
-    tuple<char *, int, int, bool> input1;
-    tuple<char *, int, int, bool> input2;
-    input1 = inputHandle(str1);
-    input2 = inputHandle(str2);
+    const char *input = str.c_str();
+    int len = str.length();
+    char *array = new char[len];
 
-    
+    int i = 0;
+
+    for (i = 0; i < len; i++)
+    {
+        if (array[i] != 45 && array[i] < 48 && array[i] > 57)
+        {
+            cout << "Invalid input!" << endl;
+            exit(0);
+        }
+        array[i] = input[i];
+    }
+    return array;
 }
 
 /// @brief Input check + handle
@@ -74,43 +86,44 @@ tuple<char *, int, int, bool> inputHandle(string str)
 
     char *arrayHead = new char(0);
     int arrayTail = 0;
-    int pLocation = -1;
+    int pLocation = 0;
 
     switch (numType)
     {
     case 1:
+    {
         // array could be pure-num char list
         arrayHead = str2charl(str);
         break;
+    }
     case 2:
-        // use pLocation to record '.'s position
+    { // use pLocation to record '.'s position
         pLocation = str.find('.');
         str.erase(pLocation, 1);
+        pLocation = str.length() - pLocation;
         arrayHead = str2charl(str);
         break;
+    }
     case 3:
-        pLocation = str.find('.');
+    {
+        int rpLocation = str.find('.');
         int eLocation = str.find('e');
-        if (!eLocation)
+        if (eLocation == string::npos)
         {
             eLocation = str.find('E');
         }
         int nLocation = str.find('-');
         // dual validity check
-        if (pLocation < eLocation && (nLocation == string::npos || (eLocation + 1) == nLocation))
+        if (rpLocation < eLocation && (nLocation == string::npos || (eLocation + 1) == nLocation))
         {
-            if (nLocation)
-            {
-                str.erase(nLocation, 1);
-            }
-            if (pLocation)
-            {
-                str.erase(pLocation, 1);
-                eLocation -= 1;
-            }
             // TODO(JustLittleFive): Should split string first
             // DONE
             string strHead = str.substr(0, eLocation);
+            if (rpLocation)
+            {
+                strHead.erase(rpLocation, 1);
+            }
+            pLocation = strHead.length() - rpLocation;
             str.erase(eLocation, 1);
             string strTail = str.substr(eLocation, str.size());
             arrayHead = str2charl(strHead);
@@ -123,42 +136,97 @@ tuple<char *, int, int, bool> inputHandle(string str)
             exit(0);
         }
         break;
+    }
     default:
+    {
         cout << "Invalid input!" << endl;
         exit(0);
     }
+    }
 
     // input check & convert: DONE
-    // OUTPUT: arryHead, arrayTail, plocation
-    tuple<char *, int, int, bool>ret = make_tuple(arrayHead, arrayTail, pLocation, isNegative);
+    // OUTPUT: arryHead, arrayTail, plocation, isNegative
+    tuple<char *, int, int, bool> ret = make_tuple(arrayHead, arrayTail, pLocation, isNegative);
     return ret;
 }
 
-char *str2charl(string str)
+/// @brief mul two input bit by bit to aviod data loss
+/// @param array1: input in char[] form
+/// @param array2: input in char[] form
+/// @return ret: string form, could it start with '.'?
+string hugeMul(char *array1, char *array2)
 {
-    const char *start = str.c_str();
-    int len = str.length();
+    char res[strlen(array1) + strlen(array2)] = {0};
 
-    char *array = new char[len];
-
-    int i = 0;
-    int l = 0;
-
-    int indexE = -1;
-    int flocate = -1;
-
-    for (i = 0, l = 0; l < len; i++, l++)
+    for (int i = strlen(array1) - 1; i >= 0; i--)
     {
-        if ((start[l] < 45 || start[l] == 47) || (start[l] > 57 && start[l] != 101))
+        for (int l = strlen(array2) - 1; l >= 0; l--)
         {
-            cout << "Invalid input!" << endl;
-            return 0;
-        }
-        array[i] = start[l];
-        if (flocate > -1)
-        {
-            flocate++;
+            // -'0' converts char to number in real
+            // by minus/add '0's ascii so we can caculate directly
+            res[i + l + 1] += (array1[i] - '0') * (array2[l] - '0');
+            res[i + l] += res[i + l + 1] / 10;
+            res[i + l + 1] %= 10;
         }
     }
-    return array;
+
+    char retP[sizeof(res)] = {0};
+    for (int i = 0; i < sizeof(res); i++)
+    {
+        retP[i] = res[i] + '0';
+    }
+
+    string ret(retP);
+    while (ret[0] == '0')
+    {
+        ret.erase(0, 1);
+    }
+
+    return ret;
+}
+
+int main(int argc, char *argv[])
+{
+    string str1 = argv[1];
+    string str2 = argv[2];
+    // cout << "input" << endl;
+    // string str1 = "";
+    // string str2 = "";
+    // cin >> str1 >> str2;
+    tuple<char *, int, int, bool> input1;
+    tuple<char *, int, int, bool> input2;
+    input1 = inputHandle(str1);
+    // cout << "input1 check" << endl;
+    input2 = inputHandle(str2);
+    // cout << "input2 check" << endl;
+    // cout << "str1: " << get<0>(input1) << endl;
+    // cout << "str2: " << get<0>(input2) << endl;
+
+    // calculate result - or +
+    bool isNegative = get<3>(input1) ^ get<3>(input2);
+    // char negative = (get<3>(input1) * get<3>(input2)) ? '-' : '';
+    // cout << "hugeMul start" << endl;
+    string resHead = hugeMul(get<0>(input1), get<0>(input2));
+    // cout << "hugeMul end" << endl;
+    int resTail = get<1>(input1) + get<1>(input2);
+    int pLocation = get<2>(input1) + get<2>(input2);
+
+    while (resHead.length() <= pLocation)
+    {
+        resHead.insert(0, "0");
+    }
+    resHead.insert(resHead.length() - pLocation, ".");
+
+    cout << str1 << " * " << str2 << " = ";
+    if (isNegative)
+    {
+        cout << '-';
+    }
+    cout << resHead;
+    if (resTail)
+    {
+        cout << 'E' << resTail;
+    }
+    cout << endl;
+    return 0;
 }
